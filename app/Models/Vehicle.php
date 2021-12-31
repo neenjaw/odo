402db97df3636f72464292c9e5c17a8fd43136c0
Tim Austin <tim@neenjaw.com>
2021-12-31 02:57:42 -0600

    add vehicle model

---
+++ app/Models/Vehicle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Uuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vehicle extends Model
+{
+    use HasFactory, Uuids;
+
+    protected $fillable = ['name', 'color', 'owner_id'];
+    protected $guarded = ['owner_id'];
+}
---
+++ database/migrations/2021_12_31_080052_create_vehicles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('color')->nullable();
+            $table->unsignedInteger('sort')->default(0);
+            $table->unsignedBigInteger('owner_id');
+            $table->index('owner_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
