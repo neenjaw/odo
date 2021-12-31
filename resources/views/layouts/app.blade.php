d0abf4c8b4c8371fe21609548a9873a5e05cf15f
Tim Austin <tim@neenjaw.com>
2021-12-31 01:04:17 -0600

    add tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,10 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <title>{{ $title }}</title>
 </head>
 <body>
   @yield('content')
-  <script src="https://unpkg.com/tailwindcss-jit-cdn"></script>
 </body>
 </html>
